da35f379d6b15a8679c618a47e531fbd293f73dc
Sergio García <sgs.garcia93@gmail.com>
2020-07-18 23:57:50 -0500

    fix(infra): Remove project creation from terraform config

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,16 +1,8 @@
-resource "google_firebase_project_location" "basic" {
-  provider    = google-beta
-
-  project     = google_firebase_project.default.project
-  location_id = var.region
-}
-
 resource "google_firebase_web_app" "basic" {
   provider = google-beta
 
-  project      = google_project.treeapp.project_id
+  project      = var.project
   display_name = "Family Tree App"
-  depends_on   = [google_firebase_project.default]
 }
 
 data "google_firebase_web_app_config" "basic" {
--- terraform/main.tf
+++ terraform/main.tf
@@ -20,16 +20,3 @@ terraform {
     }
   }
 }
-
-resource "google_project" "treeapp" {
-  provider   = google-beta
-
-  project_id = var.project
-  name       = var.project_name
-}
-
-resource "google_firebase_project" "default" {
-  provider = google-beta
-
-  project  = google_project.treeapp.project_id
-}
